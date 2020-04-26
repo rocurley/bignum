a30314d758c2733f587ea2cb5176dc7e4d157a27
Roger Curley <rocurley@gmail.com>
2020-04-25 19:01:01 -0700

    Faster add to digits

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,16 @@ extern crate proptest;
 #[cfg(test)]
 extern crate test;
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq)]
 pub struct BigInt {
     digits: Vec<u64>,
+}
+impl std::fmt::Debug for BigInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BigInt")
+            .field("digit", &format!("{:x?}", &self.digits))
+            .finish()
+    }
 }
 
 impl BigInt {
@@ -31,13 +38,26 @@ fn add_to_digits(x: u64, digits: &mut [u64]) {
     }
 }
 
+fn add_u128_to_digits(x: u128, digits: &mut [u64]) {
+    if digits.len() == 1 {
+        digits[0] += x as u64;
+        return;
+    }
+    let lsb = digits[0] as u128 + ((digits[1] as u128) << 64);
+    let (res, overflow) = lsb.overflowing_add(x);
+    digits[0] = res as u64;
+    digits[1] = (res >> 64) as u64;
+    if overflow {
+        add_to_digits(1, &mut digits[2..])
+    }
+}
+
 pub fn schoolbook_mul(l: &BigInt, r: &BigInt) -> BigInt {
     let mut digits = vec![0; l.digits.len() + r.digits.len() + 1];
     for (i, &l_digit) in l.digits.iter().enumerate() {
         for (j, &r_digit) in r.digits.iter().enumerate() {
-            let (lsb, msb) = mul_u64(l_digit, r_digit);
-            add_to_digits(lsb, &mut digits[i + j..]);
-            add_to_digits(msb, &mut digits[i + j + 1..]);
+            let prod = l_digit as u128 * r_digit as u128;
+            add_u128_to_digits(prod, &mut digits[i + j..]);
         }
     }
     BigInt { digits }.trim()
@@ -69,13 +89,23 @@ mod tests {
     fn any_bigint(range: std::ops::Range<usize>) -> impl Strategy<Value = BigInt> {
         proptest::collection::vec(any::<u64>(), range).prop_map(|digits| BigInt { digits }.trim())
     }
+    #[test]
+    fn hardcoded() {
+        let a = BigInt { digits: vec![2] };
+        let b = BigInt {
+            digits: vec![0x8000000000000000, 1],
+        };
+        let prod = schoolbook_mul(&a, &b);
+        let c = BigInt { digits: vec![0, 3] };
+        assert_eq!(prod, c);
+    }
     proptest! {
         #[test]
         fn mul_small(a in any::<u64>(), b in any::<u64>()) {
-            let a_big = BigInt{digits: vec![a]};
-            let b_big = BigInt{digits: vec![b]};
+            let a_big = BigInt{digits: vec![a]}.trim();
+            let b_big = BigInt{digits: vec![b]}.trim();
             let prod = schoolbook_mul(&a_big, &b_big);
-            let prod_pair = (prod.digits[0], prod.digits.get(1).copied().unwrap_or(0));
+            let prod_pair = (prod.digits.get(0).copied().unwrap_or(0), prod.digits.get(1).copied().unwrap_or(0));
             assert_eq!(prod_pair, mul_u64(a, b));
        }
     }
@@ -103,19 +133,18 @@ mod tests {
             assert_eq!(sum_first, sum_last);
         }
     }
+    fn random_bigint(rng: &mut rand_chacha::ChaCha8Rng, size: usize) -> BigInt {
+        let mut digits = vec![0; size];
+        for x in digits.iter_mut() {
+            *x = rng.gen();
+        }
+        BigInt { digits }
+    }
     #[bench]
     fn bench_schoolbook_mul(bench: &mut Bencher) {
         let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(0);
-        let mut a_digits = vec![0; 100];
-        for x in a_digits.iter_mut() {
-            *x = rng.gen();
-        }
-        let a = BigInt { digits: a_digits };
-        let mut b_digits = vec![0; 100];
-        for x in b_digits.iter_mut() {
-            *x = rng.gen();
-        }
-        let b = BigInt { digits: b_digits };
+        let a = random_bigint(&mut rng, 100);
+        let b = random_bigint(&mut rng, 100);
         bench.iter(|| schoolbook_mul(&a, &b));
     }
 }
