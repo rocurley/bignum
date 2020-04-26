8913dc909e9da007447865f557a4417becb597e6
Roger Curley <rocurley@gmail.com>
2020-04-26 11:00:07 -0700

    Vectorized version works, is much worse

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #![feature(test)]
+extern crate packed_simd;
+use packed_simd::u128x4;
 #[cfg(test)]
 extern crate proptest;
 #[cfg(test)]
@@ -62,6 +64,84 @@ pub fn schoolbook_mul(l: &BigInt, r: &BigInt) -> BigInt {
     }
     BigInt { digits }.trim()
 }
+
+pub fn schoolbook_mul_vec(l: &BigInt, r: &BigInt) -> BigInt {
+    let mut digits = vec![0; l.digits.len() + r.digits.len() + 1];
+    dbg!(l, r);
+    for (i, l_vec) in all_vectors(&l.digits).enumerate() {
+        for (j, r_vec) in non_overlapping_vectors(&r.digits).enumerate() {
+            let prod = r_vec * l_vec;
+            dbg!(i, j, l_vec, r_vec, prod);
+            for k in 0..4 {
+                let prod_k = prod.extract(k);
+                // Since we zero-pad
+                if prod_k != 0 {
+                    let l_index = i + k - 3;
+                    let r_index = 4 * j + k;
+                    let digits_ix = l_index + r_index;
+                    add_u128_to_digits(prod.extract(k), &mut digits[digits_ix..]);
+                }
+            }
+        }
+    }
+    BigInt { digits }.trim()
+}
+
+fn non_overlapping_vectors<'a>(slice: &'a [u64]) -> impl Iterator<Item = u128x4> + 'a {
+    slice.chunks(4).map(|chunk| {
+        let mut arr = [0; 4];
+        for (chunk_val, arr_val) in chunk.iter().zip(arr.iter_mut()) {
+            *arr_val = *chunk_val as u128;
+        }
+        u128x4::from_slice_unaligned(&arr)
+    })
+}
+fn all_vectors(slice: &[u64]) -> AllVectors {
+    if slice.len() == 0 {
+        // Skip it!
+        AllVectors { slice, end: 4 }
+    } else {
+        AllVectors { slice, end: 1 }
+    }
+}
+
+struct AllVectors<'a> {
+    slice: &'a [u64],
+    end: usize,
+}
+impl AllVectors<'_> {
+    fn get_zero_padded(&self, i: usize) -> u128 {
+        self.slice.get(i).copied().unwrap_or(0) as u128
+    }
+}
+impl<'a> Iterator for AllVectors<'a> {
+    type Item = u128x4;
+    fn next(&mut self) -> Option<Self::Item> {
+        let out = if self.end < 4 {
+            let mut arr = [0; 4];
+            for i in 0..self.end {
+                arr[i + 4 - self.end] = self.get_zero_padded(i) as u128;
+            }
+            u128x4::from_slice_unaligned(&arr)
+        } else if self.end > self.slice.len() + 3 {
+            return None;
+        } else if self.end > self.slice.len() {
+            let mut arr = [0; 4];
+            for i in self.end - 4..self.slice.len() {
+                arr[i + 4 - self.end] = self.get_zero_padded(i) as u128;
+            }
+            u128x4::from_slice_unaligned(&arr)
+        } else {
+            let mut arr = [0; 4];
+            for i in self.end - 4..self.end {
+                arr[i + 4 - self.end] = self.get_zero_padded(i) as u128;
+            }
+            u128x4::from_slice_unaligned(&arr)
+        };
+        self.end += 1;
+        Some(out)
+    }
+}
 pub fn add(l: &BigInt, r: &BigInt) -> BigInt {
     let (BigInt { digits: big }, BigInt { digits: small }) = if l.digits.len() > r.digits.len() {
         (l, r)
@@ -82,9 +162,10 @@ mod tests {
     extern crate proptest;
     extern crate rand;
     extern crate rand_chacha;
-    use crate::{add, mul_u64, schoolbook_mul, BigInt};
+    use crate::*;
     use proptest::prelude::*;
     use rand::{Rng, SeedableRng};
+    use std::collections::HashMap;
     use test::Bencher;
     fn any_bigint(range: std::ops::Range<usize>) -> impl Strategy<Value = BigInt> {
         proptest::collection::vec(any::<u64>(), range).prop_map(|digits| BigInt { digits }.trim())
@@ -133,6 +214,28 @@ mod tests {
             assert_eq!(sum_first, sum_last);
         }
     }
+    #[test]
+    fn test_schoolbook_mul_vec_hardcoded() {
+        let operands = vec![
+            (BigInt { digits: vec![] }, BigInt { digits: vec![] }),
+            (BigInt { digits: vec![] }, BigInt { digits: vec![1] }),
+            (BigInt { digits: vec![1] }, BigInt { digits: vec![1] }),
+            (BigInt { digits: vec![1] }, BigInt { digits: vec![0, 1] }),
+        ];
+        for (a, b) in operands {
+            let expected = schoolbook_mul(&a, &b);
+            let actual = schoolbook_mul_vec(&a, &b);
+            assert_eq!(expected, actual);
+        }
+    }
+    proptest! {
+        #[test]
+        fn test_schoolbook_mul_vec(a in any_bigint(0..20),b in any_bigint(0..20)) {
+            let expected = schoolbook_mul(&a, &b);
+            let actual = schoolbook_mul_vec(&a, &b);
+            assert_eq!(expected, actual);
+        }
+    }
     fn random_bigint(rng: &mut rand_chacha::ChaCha8Rng, size: usize) -> BigInt {
         let mut digits = vec![0; size];
         for x in digits.iter_mut() {
@@ -140,11 +243,63 @@ mod tests {
         }
         BigInt { digits }
     }
+    #[test]
+    fn test_all_vectors_hardcoded() {
+        let arr = [0, 1, 2, 3, 4];
+        let mut iter = all_vectors(&arr);
+        let mut tmp = [0; 4];
+        iter.next().unwrap().write_to_slice_aligned(&mut tmp);
+        assert_eq!(tmp, [0, 0, 0, 0]);
+        iter.next().unwrap().write_to_slice_aligned(&mut tmp);
+        assert_eq!(tmp, [0, 0, 0, 1]);
+        iter.next().unwrap().write_to_slice_aligned(&mut tmp);
+        assert_eq!(tmp, [0, 0, 1, 2]);
+        iter.next().unwrap().write_to_slice_aligned(&mut tmp);
+        assert_eq!(tmp, [0, 1, 2, 3]);
+        iter.next().unwrap().write_to_slice_aligned(&mut tmp);
+        assert_eq!(tmp, [1, 2, 3, 4]);
+        iter.next().unwrap().write_to_slice_aligned(&mut tmp);
+        assert_eq!(tmp, [2, 3, 4, 0]);
+        iter.next().unwrap().write_to_slice_aligned(&mut tmp);
+        assert_eq!(tmp, [3, 4, 0, 0]);
+        iter.next().unwrap().write_to_slice_aligned(&mut tmp);
+        assert_eq!(tmp, [4, 0, 0, 0]);
+        assert_eq!(iter.next(), None);
+    }
+    #[test]
+    fn test_all_vectors_empty() {
+        let arr = [];
+        let mut iter = all_vectors(&arr);
+        assert_eq!(iter.next(), None);
+    }
+    proptest! {
+        #[test]
+        fn test_all_vectors(l in 1u64..1000) {
+            let test_vector : Vec<u64> = (1..l+1).into_iter().collect();
+            let mut counts = HashMap::new();
+            for v in all_vectors(&test_vector) {
+                for i in 0..4 {
+                    *counts.entry(v.extract(i)).or_insert(0) += 1;
+                }
+            }
+            assert_eq!(*counts.get(&0).unwrap(), 12);
+            for i in test_vector {
+                assert_eq!(*counts.get(&(i as u128)).unwrap(), 4);
+            }
+        }
+    }
     #[bench]
     fn bench_schoolbook_mul(bench: &mut Bencher) {
         let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(0);
         let a = random_bigint(&mut rng, 100);
         let b = random_bigint(&mut rng, 100);
         bench.iter(|| schoolbook_mul(&a, &b));
+    }
+    #[bench]
+    fn bench_schoolbook_mul_vec(bench: &mut Bencher) {
+        let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(0);
+        let a = random_bigint(&mut rng, 100);
+        let b = random_bigint(&mut rng, 100);
+        bench.iter(|| schoolbook_mul_vec(&a, &b));
     }
 }
